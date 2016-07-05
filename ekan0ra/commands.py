5d6bcfa66e007e6e0fa5c491d6b71f3d4f468be5
Tosin Damilare James Animashaun <acetakwas@gmail.com>
2016-07-05 18:33:09 +0100

    Update commands list

--- ekan0ra/commands.py
+++ ekan0ra/commands.py
@@ -4,7 +4,10 @@
 
 
 commands = {
+    '.help' : 'list all the commands',
     '!' : 'Queue yourself to ask a question during a session',
+    '!!' : 'Remove yourself from question queue during a session',
+    '!-' : 'Remove yourself from question queue during a session',
     '.givemelogs' : 'Give you a fpaste link with the latest log',
     '.clearqueue' : 'Clear the ask question queue',
     '.next' : 'ping the person in the queue to ask question',
@@ -13,7 +16,6 @@ commands = {
     '.rm [nick]' : 'removes the nick from masters list',
     '.startclass' : 'start logging the class',
     '.endclass' : 'ends logging the class',
-    '.pingall [message]' : 'pings the message to all',
-    '.help' : 'list all the commands',
+    '.pingall [message]' : 'pings the message to all',    
     '.link [portal]' : 'Returns the link of the portal'
 }
