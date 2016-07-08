8d70b1475e437e846e5fa3654dacbfff5ae86532
Tosin Damilare James Animashaun <acetakwas@gmail.com>
2016-07-08 11:31:42 +0100

    Update test_bot

--- ekan0ra/tests/test_bot.py
+++ ekan0ra/tests/test_bot.py
@@ -12,6 +12,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspa
 # local imports
 from ekan0ra.bot import LogBot
 from ekan0ra.user import IRCUser, InvalidUserError
+from ekan0ra.commands import commands
 from config import config_modes
 
 
@@ -40,7 +41,6 @@ class BotCommandsTest(unittest.TestCase):
             '~acetakwas@41.203.71.181'
         ]
 
-
         self.assertFalse(
             mock_create_app_logger.called,
             'Create app logger was called too early')
@@ -59,9 +59,13 @@ class BotCommandsTest(unittest.TestCase):
 
     @mock.patch('ekan0ra.bot.irc.IRCClient')
     @mock.patch.object(LogBot, 'describe', auto_spec=True) # Fake links data load
-    def test_join_question_queue(self, mock_describe, mock_irc_IRCClient):
+    def test_join_question_queue_command(self, mock_describe,
+            mock_irc_IRCClient):
         self.assertListEqual([], self.bot.qs_queue) # Ensure queue is empty
         
+        command = '!'
+        self.assertIn(command, commands.keys())
+
         # Add users to queue
         for hostmask in self.validhostmasks:
             self.bot.privmsg(hostmask, self.bot.channel, '!')
@@ -69,17 +73,23 @@ class BotCommandsTest(unittest.TestCase):
             self.assertIn(IRCUser(self.bot, hostmask).nick, self.bot.qs_queue)
             self.assertEquals(IRCUser(self.bot, hostmask).nick, self.bot.qs_queue[-1])
 
-    @mock.patch('ekan0ra.bot.irc.IRCClient')
+    
     @mock.patch.object(LogBot, 'describe', auto_spec=True) # Fake links data load
-    def test_leave_question_queue(self, mock_describe, mock_irc_IRCClient):
+    @mock.patch('ekan0ra.bot.irc.IRCClient')
+    def test_leave_question_queue_command(self, mock_irc_IRCClient,
+            mock_describe):
         self.assertListEqual([], self.bot.qs_queue) # Ensure queue is empty
         
         leave_queue_commands = ('!!', '!-')
-        
-        for command in leave_queue_commands:        
+        join_queue_command = '!'
+        self.assertIn(join_queue_command, commands.keys())
+
+        for command in leave_queue_commands:
+            self.assertIn(command, commands.keys())
+
             # First, join queue
             hostmask = self.validhostmasks[0]
-            self.bot.privmsg(hostmask, self.bot.channel, '!')
+            self.bot.privmsg(hostmask, self.bot.channel, join_queue_command)
             assert(mock_describe.called)
             self.assertIn(IRCUser(self.bot, hostmask).nick, self.bot.qs_queue)
             self.assertEquals(IRCUser(self.bot, hostmask).nick, self.bot.qs_queue[-1])
@@ -98,7 +108,32 @@ class BotCommandsTest(unittest.TestCase):
         # self.assertListEqual([], self.bot.qs_queue)
         # self.assertNotEquals(IRCUser(self.bot, hostmask).nick, self.bot.qs_queue[-1])
 
+    @mock.patch('ekan0ra.bot.sys')
+    @mock.patch('ekan0ra.bot.fpaste')
+    @mock.patch('ekan0ra.bot.irc.IRCClient')
+    def test_givemelogs_command(self, mock_irc_IRCClient,
+            mock_fpaste, mock_sys):
+        command = '.givemelogs'
+        self.assertIn(command, commands.keys())
+        hostmask = self.validhostmasks[0]
+        
+        self.bot.last_log_filename = True # fake value for last_log_filename attribute
+        self.bot.privmsg(hostmask, self.bot.channel, command)
+        self.assertEquals(2, len(mock_sys.argv))
+        self.assertIn('fpaste', mock_sys.argv)
+        self.assertEquals(self.bot.last_log_filename, mock_sys.argv[1])
+        self.assertTrue(mock_fpaste.main.called)
 
+
+    @mock.patch.object(LogBot, 'say', auto_spec=True)
+    @mock.patch('ekan0ra.bot.irc.IRCClient')
+    def test_help_command(self, mock_irc_IRCClient, mock_say):
+        command = '.help'
+        self.assertIn(command, commands.keys())
+        hostmask = self.validhostmasks[0]
+
+        self.bot.privmsg(hostmask, self.bot.channel, command)
+        self.assertTrue(mock_say.called)
 
     # def test_hostmask_parsing(self):
     #     for hostmask in self.invalidhostmasks:
